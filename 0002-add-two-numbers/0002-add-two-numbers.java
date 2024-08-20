3711175b69254d25d231ec37e312822a794bcf05
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-08-20 21:35:39 +0530

    Time: 1 ms (100%), Space: 44.9 MB (5.58%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,27 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
-        int carry = 0;
+        ListNode dummy =new ListNode(-1);
+        ListNode temp=dummy;
+        int carry=0;
+        while(l1!=null || l2!=null)
+        {
+            int sum=carry;
+            if(l1!=null)
+            {
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null)
+            {
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            ListNode node= new ListNode(sum%10);
+            temp.next=node;
+            temp=temp.next;
+             carry=sum/10;
 
-        while (l1 != null || l2 != null || carry != 0) {
-            int digit1 = (l1 != null) ? l1.val : 0;
-            int digit2 = (l2 != null) ? l2.val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode newNode = new ListNode(digit);
-            tail.next = newNode;
-            tail = tail.next;
-
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
         }
+        if(carry!=0)
+        {
+            ListNode node=new ListNode(carry);
+            temp.next=node;
 
-        ListNode result = dummyHead.next;
-        dummyHead.next = null;
-        return result;
+        }
+        return dummy.next;
     }
 }
