93858351df1d536b7a227e82c2a972783f7e83aa
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-08-21 12:06:04 +0530

    Time: 0 ms (100%), Space: 44.4 MB (47.46%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,53 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-import java.util.ArrayList;
-
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) {
-            return head; // If the list is empty or has only one node, return as-is.
+            return head;
         }
 
-        ListNode temp = head;
-        ArrayList<Integer> list = new ArrayList<>();
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
 
-        // Traverse the list and collect odd-indexed node values
-        while (temp != null) {
-            list.add(temp.val);
-            if (temp.next != null) {
-                temp = temp.next.next;
-            } else {
-                break;
-            }
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;  // Update odd.next
+            even.next = even.next.next; // Update even.next
+            odd = odd.next; // Move odd to the next odd node
+            even = even.next; // Move even to the next even node
         }
 
-        // Traverse the list and collect even-indexed node values
-        ListNode temp1 = head.next;
-        while (temp1 != null) {
-            list.add(temp1.val);
-            if (temp1.next != null) {
-                temp1 = temp1.next.next;
-            } else {
-                break;
-            }
-        }
-
-        // Reassign the values from the list back to the linked list
-        int i = 0;
-        ListNode temp2 = head;
-        while (temp2 != null) {
-            temp2.val = list.get(i);
-            temp2 = temp2.next;
-            i++;
-        }
+        odd.next = evenHead; // Connect the end of odd list to the start of even list
 
         return head;
     }
