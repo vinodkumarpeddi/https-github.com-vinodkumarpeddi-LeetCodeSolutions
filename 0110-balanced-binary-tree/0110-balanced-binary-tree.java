6f3092483ba22ba43e86855d630d54695483bf82
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-09-16 19:18:20 +0530

    Time: 43 ms (5.39%), Space: 44 MB (94.07%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,23 @@
+class Solution {
+    private static int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int lf = height(root.left);
+        int rf = height(root.right);
+        return 1 + Math.max(lf, rf);
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        boolean left = isBalanced(root.left);
+        boolean right = isBalanced(root.right);
+        
+        // Correcting the logic of diff
+        boolean diff = Math.abs(height(root.left) - height(root.right)) <= 1;
+
+        return left && right && diff;
+    }
+}
