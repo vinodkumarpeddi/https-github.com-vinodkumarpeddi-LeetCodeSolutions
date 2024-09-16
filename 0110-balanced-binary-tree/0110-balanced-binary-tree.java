4e3cd06a510fcdd816fbb82c2e8f41fce132c50c
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-09-16 19:43:50 +0530

    Time: 1 ms (28.17%), Space: 44.6 MB (15.64%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,33 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private static int dfsheight(TreeNode root)
-    {
-        if(root==null)
-        {
+    private static int height(TreeNode root) {
+        if (root == null) {
             return 0;
         }
-        int lf=dfsheight(root.left);
-        if(lf==-1) return -1;
-        int rf=dfsheight(root.right);
-        if(rf==-1) return -1;
-        if(Math.abs(lf-rf)>1) return -1;
-        return Math.max(lf,rf)+1;
+        int lf = height(root.left);
+        int rf = height(root.right);
+        return 1 + Math.max(lf, rf);
     }
+
     public boolean isBalanced(TreeNode root) {
-        return dfsheight(root)!=-1;
+        if (root == null) {
+            return true;
+        }
+        boolean left = isBalanced(root.left);
+        if(left==false) return false;
+        boolean right = isBalanced(root.right);
+         if(right==false) return false;
+        
+        // Correcting the logic of diff
+        boolean diff = Math.abs(height(root.left) - height(root.right)) <= 1;
+
+        return left && right && diff;
     }
 }
