02fa34ee82032e68ff9b20e6620ec6e5bcc70b05
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-11-02 09:57:57 +0530

    Time: 1 ms (96.81%), Space: 42.2 MB (25.89%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String[] arr=sentence.split(" ");
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i].charAt(arr[i].length()-1)!=(arr[i+1].charAt(0)))
+            {
+                return false;
+            }
+        }
+        if(arr[0].charAt(0)!=arr[arr.length-1].charAt(arr[arr.length-1].length()-1))
+        {
+            return false;
+        }
+        return true;
+    }
+}
